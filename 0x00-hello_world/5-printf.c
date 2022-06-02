922eda9c4abf4952bb47b25f6a2e189f31d0c93e
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-02 14:35:55 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
-#include <stdio.h>                                                                                                                                    /**
+#include <stdio.h>
+/**
  * main - Entry point
  *
  * return (0)
  */
-int main(void)                                                                                                                                         {
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n";
 return (0);
 }
