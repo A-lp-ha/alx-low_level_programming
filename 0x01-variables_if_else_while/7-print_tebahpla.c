9b841e712108995422fc87ba4a98445ec4a557f3
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-03 14:39:55 -0700

    k

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -8,7 +8,7 @@
 int main(void)
 {
 char ddd;
-for (ddd <= 'z'; ddd = 'a'; ddd--)
+for (ddd = 'z'; ddd >= 'a'; ddd--)
 {
 putchar(ddd);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char a;
+char bb;
+for(a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+for(bb = 'a'; bb <= 'f'; bb++)
+{
+putchar(bb);
+}
+putchar('\n');
+return (0);
+}
+
