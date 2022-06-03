18bb4fef8ce7bc0699388245e702403e6fb92947
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-03 12:20:38 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char dddd;
+
+for(dddd = 'a';dddd <='z'; dddd++)
+{
+putchar(dddd);
+}
+putchar('\n');
+return (0);
+}	
