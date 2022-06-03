86efcc4f1cc35a25369fc92fec5ccb74296feecf
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-03 15:10:10 -0700

    k

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int a;
-	for (a = '0'; a <= '9'; a++)
-	{
-		putchar(a);
-	}
-	if (a != '9')
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+if (a != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
