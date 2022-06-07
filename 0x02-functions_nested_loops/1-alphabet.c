0afbe79132ebbe4afb63095174f1978b6a42fb94
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-07 10:13:47 -0700

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - check code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char dd = 'a';
+while (dd <= 'z')
+{
+_putchar('dd');
+dd++;
+}
+_putchar('\n');
+}
+int main(void)
+{
+char print_alphabet = 'll';
+printf("%s",'ll');
+return (0);
+}
