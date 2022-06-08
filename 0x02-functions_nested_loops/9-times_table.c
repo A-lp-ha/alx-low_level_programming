545e0570fdba975084b5a94a48a7bd3128c42f55
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-08 15:25:24 -0700

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 
-void times_table(void){
-	int r;
-	int c;
+void times_table(void)
+{
+int r;
+int c;
 
-	       for (c = 0; c <= 9; c++){
-	       for (r = 0; r < 10; r++){
-		       int answer = r * c;
-	if (r == 0){
+for (c = 0; c <= 9; c++)
+{
+for (r = 0; r < 10; r++)
+{
+int answer = r * c;
+if (r == 0)
+{
 _putchar(answer + '0');
-	}
-	else if (answer >= 10)
-	{
-	       	_putchar(' ');
-		_putchar(answer / 10 + '0');
-		_putchar(answer % 10 + '0');
-	}
-	else
-	{
-		_putchar(' ');
-		_putchar(',');
-		_putchar(answer + '0');
-	}
-	if (r != 9)
-	{
-		_putchar(',');
-	}
-	       }
-	       _putchar('\n');
-	       }
+}
+else if (answer >= 10)
+{
+_putchar(' ');
+_putchar(answer / 10 + '0');
+_putchar(answer % 10 + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(answer + '0');
+}
+if (r != 9)
+{
+_putchar(',');
+}
+}
+_putchar('\n');
+}
 }
