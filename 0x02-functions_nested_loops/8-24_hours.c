8146058c488cf7bbbf8e27d13729f95cc18816be
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-08 15:08:28 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,15 @@ void jack_bauer(void){
  int b;
 
  for (a = 0; a < 24; a++)
-{for ( b + 0; b < 60; b++)
+{
+	for (b + 0; b < 60; b++)
 	{
 		_putchar('0' + (a / 10));
 		_putchar('0' + (a % 10));
 	_putchar(':');
-	_putchar(b / 10));
-	_putchar(b % 10));
-	_putchar('\n'');
+	_putchar('0' + (b / 10));
+	_putchar('0' + (b % 10));
+	_putchar('\n');
 	}
 }
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,9 +4,9 @@
 void times_table(void){
 	int r;
 	int c;
-	int answer = r * c
+	int answer = r * c;
 
-	       for (c = 0; c < 10; c++){
+	       for (c = 0; c <= 9; c++){
 	       for (r = 0; r < 10; r++){
 	if (r == 0){
 _putchar(answer + '0');
