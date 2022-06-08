417f592decab28fb4ebd2a9a78267252858bed88
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-08 14:01:19 -0700

    l

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_times_table -prints a multiplication table
+ * @n: number
+ *
+ * return: numbers
+ */
+void print_times_tab (w = 0; w <= n; w++){
+	e =w * q;
+	if (e > 99){
+		_putchar('-'); 100) + '0');
+		_putchar(((e / 10) % 10) 
+		_putchar((e % 10) + '0');}if (e > 99){                                                        
+			                _putchar('-');                                                
+
