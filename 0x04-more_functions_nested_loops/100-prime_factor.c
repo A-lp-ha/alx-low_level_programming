c5c1bd6667357cc2e587cdf32dc7c34086521966
A-lp-ha <njugunamwaura14@gmail.com>
2022-06-10 14:24:30 -0700

    l

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,12 +11,14 @@ int main(void)
 	long int num = 612852475143;
 	long int q;
 
-	for (q = 1; q <= num; q++)
+	for (q = 2; q <= num; q++)
 	{
 		if (num % q == 0)
 		{
-			printf("the prime are: %ld\n", q);
+			num = num / q;
 		}
-	}
+	}	
+		printf("the prime are: %ld\n", q);
+	
 	return (0);
 }
