4e4d6b2846822f8cda59125b01a879898599d81e
A-lp-ha <njugunamwaura14@gmail.com>
2022-07-15 03:25:54 -0700

    k

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_list - prints all elements
+* @h: pointer
+* Return: always 0
+*/
+
+size_t print_list(const list_t *h)
+{
+const list_t *tmp;
+unsigned int i;
+
+tmp = h;
+for (i = 0; tmp; i++)
+{
+printf("[%u] %s\n", tmp->len, tmp->str);
+tmp = tmp->next;
+}
+return (i);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* list_len - finds the number of elements
+@h: pointer
+Return: always 0
+*/
+
+size_t list_len(const list_t *h)
+{
+const list_t *tmp;
+unsigned int i;
+
+tmp = h;
+for (i = 0; tmp; i++)
+tmp = tmp->next;
+return (i);
+}
